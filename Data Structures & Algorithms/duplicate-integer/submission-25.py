233e45efbebe2914887fdd22c1bb898241da255e
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2026-08-03 18:56:46 -0700

    Add: duplicate-integer - submission-25

---
+++ Data Structures & Algorithms/duplicate-integer/submission-25.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen:
+                seen[num] +=1
+                return True
+            else:
+                seen[num] = 1
+                
+        return False
+                
+
+        
+
+        
