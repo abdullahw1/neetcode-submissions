da494317214d2c449ef0a611bc59e1f1dbe854af
abdullahw1 <90009221+abdullahw1@users.noreply.github.com>
2026-08-15 20:24:12 -0700

    Add: python-list-comprehension - submission-0

---
+++ Python Coding Interviews/python-list-comprehension/submission-0.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+def create_list_of_odds(n: int) -> List[int]:
+    arr=range(1, n + 1)
+    res = [i for i in arr if i%2 != 0]
+    return res
+
+
+# do not modify below this line
+print(create_list_of_odds(1))
+print(create_list_of_odds(5))
+print(create_list_of_odds(6))
+print(create_list_of_odds(10))
